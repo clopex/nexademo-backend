89402f10c0fa5dd94248f967d4c6bdaf3278ac5a
Adis Mulabdic <adis.mulabdic@gmail.com>
2026-02-26 22:59:06 +0100

    Update User table

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe, googleLogin, appleLogin } = require('../controllers/authController');
+const { register, login, getMe, googleLogin, appleLogin, updateProfile } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/register', register);
@@ -8,5 +8,6 @@ router.post('/login', login);
 router.get('/me', authMiddleware, getMe);
 router.post('/google', googleLogin);
 router.post('/apple', appleLogin);
+router.put('/profile', authMiddleware, updateProfile);
 
 module.exports = router;
