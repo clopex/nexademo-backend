0fcb4778d35b48351ab9c153d5726970a9952f5f
Adis Mulabdic <adis.mulabdic@gmail.com>
2026-02-27 13:14:29 +0100

    Small change

--- src/routes/tips.js
+++ src/routes/tips.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getDailyTip } = require('../controllers/tipsController');
+const { getDailyTip, deleteTodayTip } = require('../controllers/tipsController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/daily', authMiddleware, getDailyTip);
